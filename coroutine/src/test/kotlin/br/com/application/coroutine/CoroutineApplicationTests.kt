76398c236619f308be0bc57bbcfb2383ef7e4c47
Ale Rodrigues <alessandraaraujo@live.com>
2021-12-23 04:57:50 -0300

    Create infrastructure

---
+++ coroutine/src/test/kotlin/br/com/application/coroutine/CoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.application.coroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
